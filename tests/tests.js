f8a4f75fb49b5d0b024fd2ecee6430e551898f8d
sschulz <s.schulz@searchmetrics.com>
2015-06-25 11:41:57 +0200

    first test script

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,16 @@
 /**
  * Created by nerdbynature on 25.06.15.
  */
+
+var should = require("should");
+var assert = require("assert");
+var threeSeconds = require("seconds");
+
+describe('Test Framework', function () {
+    it('should have mocha installed and running.', function () {
+        assert.equal(true, false);
+    })
+    it('should have should installed and running.', function () {
+        true.should.eql(true);
+    })
+})
