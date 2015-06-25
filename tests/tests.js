79d2609938dc43970b8a632ac2d31f9913fefbfa
sschulz <s.schulz@searchmetrics.com>
2015-06-25 16:27:34 +0200

    enhance test script

--- tests/tests.js
+++ tests/tests.js
@@ -8,9 +8,25 @@ var threeSeconds = require("seconds");
 
 describe('Test Framework', function () {
     it('should have mocha installed and running.', function () {
-        assert.equal(true, false);
+        assert.equal(true, true);
     })
     it('should have should installed and running.', function () {
         true.should.eql(true);
+    })
+})
+
+describe('Async Test', function () {
+    var result = false;
+    beforeEach(function (done) {
+        setTimeout(function () {
+            result = true;
+            // complete async beforeEach
+            done();
+        }, 200)
+    });
+
+    it('should not be so hard to make it easier.', function (done) {
+        result.should.eql(true);
+        done();
     })
 })
